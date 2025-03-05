abaf53b6099dc0b7dc5fb9e43b66705788fc06ca
unknown <bsuraj23@gmail.com>
2025-03-05 07:56:17 +0100

    removing other functions

--- ZugData/src/main/java/com/example/ZugData/model/PlatformModel.java
+++ ZugData/src/main/java/com/example/ZugData/model/PlatformModel.java
@@ -28,34 +28,8 @@ public class PlatformModel {
 
 	}
 
-	//parameterized constructor
-	public PlatformModel(int zugId, String zugProfile, String zugDesc, Integer zugExperience, List<String> zugSkills) {
-		super();
-		this.RouteId = RouteId;
-		this.RouteProfile = RouteProfile;
-		this.RouteDesc = RouteDesc;
-
-	}
-
-    public void setZugId(int zugId) {
-		this.RouteId = zugId;
-	}
-
-    public void setZugProfile(String zugProfile) {
-		this.RouteProfile = zugProfile;
-	}
-
-    public void setZugDesc(String zugDesc) {
-		this.RouteDesc = zugDesc;
-	}
 
 
-
-	@Override
-	public String toString() {
-		return "ZugModel [zugId=" + RouteId + ", zugProfile=" + RouteProfile + ", zugDesc=" + RouteDesc + ", zugExperience=" + "]";
-
-	}
-	
+d
 
 }
