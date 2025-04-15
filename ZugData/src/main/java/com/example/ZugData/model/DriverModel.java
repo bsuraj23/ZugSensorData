6eaf95149f87df7ec6019b9f367216f08a861788
unknown <bsuraj23@gmail.com>
2025-04-15 20:16:38 +0200

    adding driver related services

---
+++ ZugData/src/main/java/com/example/ZugData/model/DriverModel.java
@@ -0,0 +1,59 @@
+package com.example.ZugData.model;
+
+
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Getter
+@Data
+
+@Component
+public class DriverModel {
+
+
+    //getters and setters
+    private int driverId;
+	private String driverProfile;
+	private String driverDesc;
+	private String driverAddress;
+
+
+	//default constructor
+	public DriverModel() {
+
+	}
+
+	//parameterized constructor
+	public DriverModel(int zugId, String zugProfile, String zugDesc, Integer zugExperience, List<String> zugSkills) {
+		super();
+		this.zugId = zugId;
+		this.zugProfile = zugProfile;
+		this.zugDesc = zugDesc;
+
+	}
+
+    public void setZugId(int zugId) {
+		this.zugId = zugId;
+	}
+
+    public void setZugProfile(String zugProfile) {
+		this.zugProfile = zugProfile;
+	}
+
+    public void setZugDesc(String zugDesc) {
+		this.zugDesc = zugDesc;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "ZugModel [zugId=" + zugId + ", zugProfile=" + zugProfile + ", zugDesc=" + zugDesc + ", zugExperience="
+				+ zugExperience + ", zugSkills=" + zugSkills + "]";
+	}
+//
+
+}
